cb05ad6f8d09550ab2629ce89087819e899d1333
iseungchul <tnsrb950@gmail.com>
2023-06-27 06:25:28 +0900

    null값 제외 JsonInclude 어노테이션

--- src/main/java/com/sparta/blogspring/Dto/BlogResponseDto.java
+++ src/main/java/com/sparta/blogspring/Dto/BlogResponseDto.java
@@ -1,10 +1,12 @@
 package com.sparta.blogspring.Dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.sparta.blogspring.entity.Blog;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL) //null값들 필드 제외
 public class BlogResponseDto {
     private Boolean success;
     private Long id; //구별 식별자
